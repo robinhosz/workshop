127fe182e62831b98c7307adf44ba99433401ca2
José Robson <joserobsonsiqueira23@hotmail.com>
2022-07-03 15:41:41 -0300

    Simple query with query methods

--- src/main/java/com/spring/workshop/resources/PostResource.java
+++ src/main/java/com/spring/workshop/resources/PostResource.java
@@ -1,13 +1,13 @@
 package com.spring.workshop.resources;
 
 import com.spring.workshop.domain.Post;
+import com.spring.workshop.resources.util.URL;
 import com.spring.workshop.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -23,6 +23,11 @@ public class PostResource {
         return ResponseEntity.ok().body(obj);
     }
 
-
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
 
 }
--- src/main/java/com/spring/workshop/service/PostService.java
+++ src/main/java/com/spring/workshop/service/PostService.java
@@ -6,6 +6,7 @@ import com.spring.workshop.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,5 +22,8 @@ public class PostService {
         return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
+    public List<Post> findByTitle(String text) {
+        return repository.findByTitleContainingIgnoreCase(text);
+    }
 
 }
