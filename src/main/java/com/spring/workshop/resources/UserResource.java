597129fbd77c246781bf09f2dd5b99e1f68b2d36
José Robson <joserobsonsiqueira23@hotmail.com>
2022-07-03 01:07:20 -0300

    Conected mongodb

--- src/main/java/com/spring/workshop/resources/UserResource.java
+++ src/main/java/com/spring/workshop/resources/UserResource.java
@@ -1,25 +1,24 @@
 package com.spring.workshop.resources;
 
 import com.spring.workshop.domain.User;
+import com.spring.workshop.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
-        User maria = new User("1","Maria", "maria@gmail.com");
-        User alex = new User("2","Alex", "alex@gmail.com");
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(maria, alex));
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
