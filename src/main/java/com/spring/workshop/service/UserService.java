c9cc61b7059e75b4deafa2e613297e7bfc464bfa
José Robson <joserobsonsiqueira23@hotmail.com>
2022-07-03 01:43:17 -0300

    Exceptions

--- src/main/java/com/spring/workshop/service/UserService.java
+++ src/main/java/com/spring/workshop/service/UserService.java
@@ -2,6 +2,7 @@ package com.spring.workshop.service;
 
 import com.spring.workshop.domain.User;
 import com.spring.workshop.repository.UserRepository;
+import com.spring.workshop.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +16,13 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        User user = repository.findById(id).orElse(null);
+        if(user == null) {
+            throw new ObjectNotFoundException("Objeto não encontrado");
+        }
+        return user;
     }
 }
